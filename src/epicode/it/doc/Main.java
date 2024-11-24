06c6fc87eca25b27a84fbe1d1ef6c6101b553a2f
mauro_larese <mauro.larese@omnys.com>
2024-11-24 16:31:05 +0100

    Initial commit

---
+++ src/epicode/it/doc/Main.java
@@ -0,0 +1,13 @@
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Main {
+    private static final Logger LOGGER = LoggerFactory.getLogger(Main.class);
+
+    public static void main(String[] args) {
+
+        LOGGER.info("Logger mio");
+        LOGGER.debug("Logger mio debug");
+        LOGGER.trace("Logger mio trace");
+    }
+}
